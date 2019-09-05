9003eb95209021a0302688a4de618da82331a660
galbeiroc <alcresgu_1989@hotmail.com>
2019-09-05 17:30:58 -0500

    Axios installation to make requests to the server.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import React, {useState, useEffect} from 'react';
 import FormApartment from './components/FormApartment';
-import ListApartment from './components/ListApartment'
-import './App.css';
+import ListApartment from './components/ListApartment';
+import axios from 'axios';
 
-function App() {
+import './App.css';
+import { async } from 'q';
+
+const App = () => {
+  const [apartment, setApartment] = useState([])
+
+  const getApartment = async () => {
+    try {
+      const res = await axios.get('http://localhost:3100/apartments');
+      if(!res) {
+        console.log('Error Getting Apartments');
+      }
+      setApartment(res.data)
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+
+
   return (
     <div className="container">
         <div className="apartmentForm">
